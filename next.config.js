25e8f588d0d234683e5af5b6175867ea42d760b4
trautztod <68370514+trautztod@users.noreply.github.com>
2020-07-22 01:28:57 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   publicRuntimeConfig: {
     APP_NAME: "CRUXSTATION",
     APP_API_DEVELOPMENT: "http://localhost:3000",
-    APP_API_PRODUCTION: "https://fe1-cs.herokuapp.com",
+    APP_API_PRODUCTION: "https://crux-station.herokuapp.com",
     API_DEVELOPMENT: "http://localhost:7331/apiv1",
     API_PRODUCTION: "https://be1-cs.herokuapp.com/apiv1",
     PRODUCTION: true,
